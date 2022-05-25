7f67bdf7316e7a0ad61e3d7482948fad0a1dc534
Ian McCallum <ianfmc@gmail.com>
2022-05-25 14:41:51 -0700

    Update to build 23

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -208,7 +208,7 @@ class _LoginPageState extends State<LoginPage> {
                 padding: EdgeInsets.only(left: 30.0, right: 30.0),
                 child: Center(
                   child:
-                      Text('v0.8.7 Build 21', style: TextStyle(fontSize: 12)),
+                      Text('v0.8.7 Build 23', style: TextStyle(fontSize: 12)),
                 ),
               ),
               SizedBox(height: 150)
