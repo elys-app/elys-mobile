533db38ffb4f29d6d8e14d5e43f8974e87c77bf3
Ian McCallum <ianfmc@gmail.com>
2022-06-10 18:10:50 -0700

    Update to Build 47.

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -205,7 +205,7 @@ class _LoginPageState extends State<LoginPage> {
                 child: const Text('Forgot Password?'),
               ),
               Padding(
-                padding: EdgeInsets.only(left: 30.0, right: 30.0),
+                padding: EdgeInsets.only(left: 30.0, right: 30.0, bottom: 0.0),
                 child: Center(
                   child:
                       Text('v0.8.13 Build 47', style: TextStyle(fontSize: 12)),
